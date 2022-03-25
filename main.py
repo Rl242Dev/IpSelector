7b38faf1cc8286660757ae803bdb2c74a79cac8c
Rl242Dev <74558778+Rl242Dev@users.noreply.github.com>
2022-03-25 13:26:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import mysql.connector
 from mysql.connector import Error
-from data import IpAvailable
+from data import IpAvailable # Import the array from Data.py
 
 db = mysql.connector.connect(host='localhost',
-                             database='IpData',
+                             database='****',
                              user='rl242',
-                             password='31#Nigi2'
+                             password='*****'
                              )
 
-mycursor = db.cursor()
+mycursor = db.cursor() # Create a cursor in mysql
 
-mycursor.execute("USE IpData;")
+mycursor.execute("USE ***;") # Use your database 
     
-def search_ip(string):
+def search_ip(string): # Name of the person you want to search the ip
     if string in (IpAvailable):
-        mycursor.execute("SELECT ip FROM IpList WHERE name = '"+string+"' LIMIT 1;" )
-        myresult = mycursor.fetchall()
+        mycursor.execute("SELECT ip FROM IpList WHERE name = '"+string+"' LIMIT 1;" ) # Search in the database the name of the perseon + ip
+        myresult = mycursor.fetchall() # Create a var for the result
         for x in myresult:
             print(x)
     else:
-        print('Error 404, Ip not available')
+        print('Error 404, Ip not available') # Print error if the name of the person isn't in the database
