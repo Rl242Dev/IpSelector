c95e3d3c34f6fcbf9ca308dd03054109c7952a1f
Rl242Dev <74558778+Rl242Dev@users.noreply.github.com>
2022-03-25 13:27:51 +0100

    Update data.py

--- data.py
+++ data.py
@@ -10,13 +10,14 @@ db = mysql.connector.connect(host='localhost',
 
 mycursor = db.cursor()
 
-sql = "INSERT INTO ** (column_1, column_2) VALUES (%s, %s)"
+start = "USE ***;" # Use your database
+sql = "INSERT INTO ** (column_1, column_2) VALUES (%s, %s)" # Insert the data in the database via python
 val = [
-  ('****', '***'),
-  ('****', '***'),
+  ('****', '***'), # Value 1
+  ('****', '***'), # Value 2
 ]
 
-mycursor.executemany(sql, val)
+mycursor.executemany(start, sql, val) # Execute Each Command
 
 db.commit()                             
 
